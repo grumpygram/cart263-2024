151ae5e9032b197aedbe08f606872853280b01cc
grumpygram <144966696+grumpygram@users.noreply.github.com>
2024-02-08 17:03:31 -0500

    Working on voices jam

--- Projects/voices-jam/js/script.js
+++ Projects/voices-jam/js/script.js
@@ -9,19 +9,53 @@
 
 "use strict";
 
-//Preload
-function preload() {
+const speechRecognizer = new p5.SpeechRec();
+const speechSynthesizer = new p5.Speech();
 
-}
+
+let spokenWords = [`i`, `need`, `you`, `to`, `speak`, `now`];
+let rapLyrics = `?`;
 
 
 //Setup
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    //speechRecognizer.continuous = false;
+    //speechRecognizer.onResult = handleSpeechInput;
+    //speechRecognizer.start();
 
 }
 
 
 //Draw
 function draw() {
+    background(30, 0, 50);    
+
+    textSize(32);
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text(rapLyrics, width/2, height/2);
+
+    console.log(rapLyrics);
+    rapIt();
+}
+
+//Making words that are recognized into the lyrics
+/*function handleSpeechInput() {
+    currentSpeech = speechRecognizer.resultString;
+
+}*/
+
+//Making the synthesizer repeat the words back in a rhythm
+function rapIt() {
+    rapLyrics = spokenWords;
+    speechSynthesizer.speak(rapLyrics);
+
+}
+
+//Mousepressed
+function mousePressed() {
+    rapIt();
 
 }
