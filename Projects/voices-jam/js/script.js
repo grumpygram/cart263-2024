287d195273b51162e388e391c971572ad3c24f0f
grumpygram <144966696+grumpygram@users.noreply.github.com>
2024-02-08 23:07:14 -0500

    working...

--- Projects/voices-jam/js/script.js
+++ Projects/voices-jam/js/script.js
@@ -12,50 +12,61 @@
 const speechRecognizer = new p5.SpeechRec();
 const speechSynthesizer = new p5.Speech();
 
+let currentWords = [];
+let repeatingWords = [];
 
-let spokenWords = [`i`, `need`, `you`, `to`, `speak`, `now`];
-let rapLyrics = `?`;
+let currentWordFill = {
+    r: 0,
+    y: 0,
+    b: 0
+}
 
 
 //Setup
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    //speechRecognizer.continuous = false;
-    //speechRecognizer.onResult = handleSpeechInput;
-    //speechRecognizer.start();
+    speechRecognizer.continuous = true;
+    speechRecognizer.interimResults = true;
+    speechRecognizer.onResult = handleSpeechInput;
+    speechRecognizer.start();
 
 }
-
 
 //Draw
 function draw() {
     background(30, 0, 50);    
+}
+
+//Putting words that are recognized into the spoken words array
+function handleSpeechInput() {
+
+    let spokenWords = speechRecognizer.resultString;
+
+    for (let i = 0; i < spokenWords.length; i++) {
+        currentWords.push(spokenWords[i]);
+    }
+
+    console.log(currentWords);
+
+
+
+}
+
+//Displaying the words that are in the array
+function addWords() {
+    currentWordFill.r = random(50, 255);
+    currentWordFill.g = random(50, 255);
+    currentWordFill.b = random(50, 255);
 
     textSize(32);
     fill(255);
     textAlign(CENTER, CENTER);
-    text(rapLyrics, width/2, height/2);
-
-    console.log(rapLyrics);
-    rapIt();
-}
-
-//Making words that are recognized into the lyrics
-/*function handleSpeechInput() {
-    currentSpeech = speechRecognizer.resultString;
-
-}*/
-
-//Making the synthesizer repeat the words back in a rhythm
-function rapIt() {
-    rapLyrics = spokenWords;
-    speechSynthesizer.speak(rapLyrics);
+    text(currentWords, width/2, height/2);
 
 }
 
 //Mousepressed
 function mousePressed() {
-    rapIt();
 
 }
