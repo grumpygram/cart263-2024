7077eb884b6cee5298f734ea8043b0afa8d40cc9
grumpygram <144966696+grumpygram@users.noreply.github.com>
2024-02-11 18:37:50 -0500

    Updated the description haha

--- Projects/voices-jam/js/script.js
+++ Projects/voices-jam/js/script.js
@@ -2,9 +2,10 @@
  * Voices Jam
  * Graeme Peters
  * 
- * This is a project where I will attempt to make an 
- * uncooperative French robot that will pretend to not 
- * understand what you are saying to it.
+ * This is a project where I will attempt to make a 
+ * program that cuts blocks of speech into 5-word 
+ * sequences and then spits those out at random, making
+ * it somewhat difficult to understand.
  */
 
 "use strict";
