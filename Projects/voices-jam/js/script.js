b1087e86c9d5383b73f8344a1cb8504f63f69ad3
grumpygram <144966696+grumpygram@users.noreply.github.com>
2024-02-10 23:12:23 -0500

    Trying to make the words into groups of 5

--- Projects/voices-jam/js/script.js
+++ Projects/voices-jam/js/script.js
@@ -9,48 +9,14 @@
 
 "use strict";
 
-const speechRecognizer = new p5.SpeechRec();
-const speechSynthesizer = new p5.Speech();
+/*
 
-let currentWords = [];
-let repeatingWords = [];
-
-let currentWordFill = {
-    r: 0,
-    y: 0,
-    b: 0
-}
-
-
-//Setup
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-
-    speechRecognizer.continuous = true;
-    speechRecognizer.interimResults = true;
-    speechRecognizer.onResult = handleSpeechInput;
-    speechRecognizer.start();
-
-}
 
 //Draw
-function draw() {
-    background(30, 0, 50);    
-}
+function draw() {   
 
-//Putting words that are recognized into the spoken words array
-function handleSpeechInput() {
-
-    let spokenWords = speechRecognizer.resultString;
-
-    for (let i = 0; i < spokenWords.length; i++) {
-        currentWords.push(spokenWords[i]);
-    }
-
-    console.log(currentWords);
-
-
-
+    addWords();
+    sayIt();
 }
 
 //Displaying the words that are in the array
@@ -66,7 +32,120 @@ function addWords() {
 
 }
 
-//Mousepressed
-function mousePressed() {
+*/
+
+//Opening stuff
+    let backgroundFill = {
+        r: 30,
+        g: 0,
+        b: 50
+    };
+    let button = {
+        x: 0,
+        y: 0,
+        size: 100,
+        fill: {
+            r: 255,
+            g: 20,
+            b: 20
+        }
+    }
+    let currentWordFill = {
+        r: 0,
+        y: 0,
+        b: 0
+    }
+    
+    //p5.Speech stuff
+    const speechRecognizer = new p5.SpeechRec();
+    const speechSynthesizer = new p5.Speech();
+    let recordMe = false;
+
+    //Arrays
+    let currentWords = [];
+    let repeatingWords = [];
+
+
+//Setup
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    button.x = width/2;
+    button.y = height/2;
+
+    speechRecognizer.continuous = true;
+    speechRecognizer.interimResults = true;
+    speechRecognizer.onEnd = handleSpeechInput;
+    speechSynthesizer.interrupt = true;
+}
+
+//Draw
+function draw() {
+    background(backgroundFill.r, backgroundFill.g, backgroundFill.b);
+
+    push();
+    fill(button.fill.r, button.fill.g, button.fill.b);
+    ellipse(button.x, button.y, button.size);
+    pop();
+}
+
+//Putting words that are recognized into the spoken words array
+function handleSpeechInput() {
+
+    //Adding spoken words to the current sentence array
+    let lowerCaseResult = speechRecognizer.resultString.toLowerCase();
+    let parts = lowerCaseResult.split(` `)
+
+    for (let i = 0; i < currentWords.length; i++) {
+        currentWords.push(parts[i]);
+
+        if (currentWords.length === 5) {
+            repeatingWords.push(currentWords.slice());
+            currentWords = [];
+        }
+    }
+}
+
+function talkBack() {
+    speechSynthesizer.speak(currentWords);
 
 }
+
+//Mousepressed
+function mousePressed() {
+    //Distance between mouse and button
+    let d1 = dist(mouseX, mouseY, button.x, button.y);
+
+    if (recordMe === false && d1 <= button.size/2) {
+        //Starting recording
+        speechRecognizer.start();
+
+        //Setting variable
+        recordMe = true;
+        console.log(`started recording`)
+        return;
+    }
+    if (recordMe && d1 <= button.size/2) {
+        //Stopping recording
+        speechRecognizer.stop();
+
+        //Resetting variable
+        recordMe = false;
+
+        //Moving button
+        console.log(`stopped recording`)
+        button.x = random(0 + button.size/2, width - button.size/2);
+        button.y = random(0 + button.size/2, height - button.size/2);
+        talkBack();
+        return
+    }
+}
+
+/*
+  // Displaying current group
+  textAlign(CENTER);
+  textSize(24);
+  fill(0);
+  if (currentWords.length > 0) {
+    text(currentWords.join(" "), width/2, height/2);
+  }*/
