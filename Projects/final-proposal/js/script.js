a48fddde5b004c57b1aeabac51492f03eed0d98d
grumpygram <144966696+grumpygram@users.noreply.github.com>
2024-04-05 09:21:48 -0400

    Working on proposal

--- Projects/final-proposal/js/script.js
+++ Projects/final-proposal/js/script.js
@@ -9,27 +9,15 @@
 
 "use strict";
 
+/*
 let state = `loading`; //loading, running
-
-let img;
 
 //The model
 let modelName = `CocoSsd`;
 let cocossd;
 
-//The button
-let button = {
-    x: 0,
-    y: 0,
-    size: 100,
-    fill: {
-        r: 255,
-        g: 20,
-        b: 20
-    }
-}
-
-let predictions = undefined;
+let video;
+let thePic = undefined;
 
 //PRELOAD
 function preload() {
@@ -39,24 +27,18 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    button.x = width/2;
-    button.y = height/2;
-
     video = createCapture(VIDEO);
     video.hide();
 
     //Start the CocoSsd
     cocossd = ml5.objectDetector(`cocossd`, modelReady);
 
-
 }
 
+//Once the model is loaded
 function modelReady() {
-    if (state === `loading`) {
-        state = running;
-    }
+    state = `running`;
 }
-
 
 //DRAW
 function draw() {
@@ -67,20 +49,118 @@ function draw() {
     if (state = `running`) {
         running();
     }
+}
 
+function takeAPic() {
+    thePic = video;
 }
 
 //LOADING
 function loading() {
     background(0);
 
-    push();
-    fill(button.fill.r, button.fill.g, button.fill.b);
-    ellipse(button.x, button.y, button.size);
-    pop();
 }
 
 //RUNNING
 function running() {
     background(255, 0, 0);
+
+    fill(255);
+    ellipse(200, 200, 100);
+
+    image(thePic, 0, 0);
+
+}
+
+function mousePressed() {
+    if (state === `running`) {
+        takeAPic();
+    }
+}
+
+
+
+//stuff I need
+let video;
+let button;
+let picture = undefined;
+let modelName = CocoSsd;
+
+//SETUP
+function setup() {
+    createCanvas(800, 600);
+
+    video = createCapture(VIDEO);
+    video.size(800, 600);
+    video.hide();
+
+    button = createButton(`take a pic`);
+    button.position(0, 600);
+
+    button.mousePressed(sayCheese);
+}
+
+function sayCheese() {
+    picture = video;
+}
+
+//DRAW
+function draw() {
+    background(0);
+
+    image(picture, 0, 0);
+
+}
+*/
+
+// Copyright (c) 2020 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+BodyPix
+=== */
+
+let bodypix;
+let video;
+let segmentation;
+
+const options = {
+  outputStride: 8, // 8, 16, or 32, default is 16
+  segmentationThreshold: 0.3, // 0 - 1, defaults to 0.5
+};
+
+function preload() {
+  bodypix = ml5.bodyPix(options);
+}
+
+function setup() {
+  createCanvas(320, 240);
+  // load up your video
+  video = createCapture(VIDEO, videoReady);
+  video.size(width, height);
+  
+}
+
+function videoReady() {
+  bodypix.segment(video, gotResults);
+}
+
+function draw() {
+  background(0);
+  if (segmentation) {
+    image(segmentation.backgroundMask, 0, 0, width, height);
+    console.log(segmentation);
+  }
+}
+
+function gotResults(error, result) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  segmentation = result;
+  bodypix.segment(video, gotResults);
 }
