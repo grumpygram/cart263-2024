b4094e810bf399268f2fb37e8285a1232af30452
grumpygram <144966696+grumpygram@users.noreply.github.com>
2024-04-22 00:56:09 -0400

    final almost done

--- Projects/final-proposal/js/script.js
+++ Projects/final-proposal/js/script.js
@@ -10,115 +10,6 @@
 "use strict";
 
 /*
-let state = `loading`; //loading, running
-
-//The model
-let modelName = `CocoSsd`;
-let cocossd;
-
-let video;
-let thePic = undefined;
-
-//PRELOAD
-function preload() {
-}
-
-//SETUP
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-
-    video = createCapture(VIDEO);
-    video.hide();
-
-    //Start the CocoSsd
-    cocossd = ml5.objectDetector(`cocossd`, modelReady);
-
-}
-
-//Once the model is loaded
-function modelReady() {
-    state = `running`;
-}
-
-//DRAW
-function draw() {
-    //The states
-    if (state = `loading`) {
-        loading();
-    }
-    if (state = `running`) {
-        running();
-    }
-}
-
-function takeAPic() {
-    thePic = video;
-}
-
-//LOADING
-function loading() {
-    background(0);
-
-}
-
-//RUNNING
-function running() {
-    background(255, 0, 0);
-
-    fill(255);
-    ellipse(200, 200, 100);
-
-    image(thePic, 0, 0);
-
-}
-
-function mousePressed() {
-    if (state === `running`) {
-        takeAPic();
-    }
-}
-
-
-
-//stuff I need
-let video;
-let button;
-let picture = undefined;
-let modelName = CocoSsd;
-
-//SETUP
-function setup() {
-    createCanvas(800, 600);
-
-    video = createCapture(VIDEO);
-    video.size(800, 600);
-    video.hide();
-
-    button = createButton(`take a pic`);
-    button.position(0, 600);
-
-    button.mousePressed(sayCheese);
-}
-
-function sayCheese() {
-    picture = video;
-}
-
-//DRAW
-function draw() {
-    background(0);
-
-    image(picture, 0, 0);
-
-}
-*/
-
-///////
-
-
-
-/*
-
 let img;
 
 // We will hold the totals for our color values here
@@ -142,11 +33,11 @@ function setup() {
   img.loadPixels();
 
   // Loop through the pixels X and Y
-  for (let y = 0; y < img.height; y++) {
-    for (let x = 0; x < img.width; x++) {
+  for (let i = 0; i < img.height; i++) {
+    for (let j = 0; j < img.width; j++) {
 
       // Calculate the pixel index
-      const index = (y * img.width + x) * 4;
+      const index = (i * img.width + j) * 4;
 
       // Sum the red, green, and blue values
       avgRed += img.pixels[index + 0];
@@ -155,7 +46,6 @@ function setup() {
 
     }
   }
-
 
   // We're finished working with pixels so update them
   img.updatePixels();
@@ -184,4 +74,253 @@ function draw() {
 
 
 }
+
+
+///////////////////////
+
+
+let img;
+let poseNet;
+
+function preload() {
+  // load an image for pose detection
+  img = loadImage('assets/images/romane.jpg');
+}
+
+function setup() {
+  createCanvas(604, 806);
+  img.resize(604,0);
+
+  image(img, 0, 0);
+
+  poseNet = ml5.poseNet(modelReady);
+}
+
+// when poseNet is ready, do the detection
+function modelReady() {
+  // If/When a pose is detected, poseNet.on('pose', ...) will be listening for the detection results 
+  poseNet.on('pose', gotPoses);
+
+  // When the model is ready, run the singlePose() function...
+  poseNet.multiPose(img);
+}
+
+function gotPoses(poses) {
+  if (poses.length > 0) {
+    drawSkeleton(poses);
+    drawKeypoints(poses);
+    console.log(poses);
+  }
+}
+
+// The following comes from https://ml5js.org/docs/posenet-webcam
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints(poses) {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255);
+        stroke(20);
+        strokeWeight(4);
+        ellipse(round(keypoint.position.x), round(keypoint.position.y), 8, 8);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton(poses) {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255);
+      strokeWeight(1);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
+}
 */
+
+//Pose stuff
+let img;
+let poseNet;
+let chestArea;
+let poses = [];
+
+let upperX = [];
+let upperY = [];
+let lowerX = [];
+let lowerY = [];
+
+//Colour determination stuff
+let avgRedUpper = 0;
+let avgGreenUpper = 0;
+let avgBlueUpper = 0;
+
+let avgRedLower = 0;
+let avgGreenLower = 0;
+let avgBlueLower = 0;
+
+//PRELOAD
+function preload() {
+  img = loadImage('assets/images/graeme2.jpg');
+}
+
+//SETUP
+function setup() {
+  //pixel density
+  pixelDensity(1);
+  //resize image
+  img.resize(600, 0);
+  // Loading the pixels
+  img.loadPixels();
+  //canvas height is set to image height post-resize
+  createCanvas(600, img.height);
+
+  //background and display image
+  background (50);
+  image(img, 0, 0);
+
+  //Calling Posenet
+  poseNet = ml5.poseNet(img, modelLoaded);
+}
+
+//MODEL LOADED
+function modelLoaded() {
+  console.log(`model loaded`);
+
+  //When it detects poses, call gotPoses function that will make the chest
+  poseNet.on(`pose`, gotPoses);
+  poseNet.singlePose(img);
+}
+
+//GOT POSES
+function gotPoses(pose) {
+    poses = pose;
+
+  if (poses) {
+    drawChest(poses);
+    drawLegs(poses);
+    console.log(poses);
+  }
+}
+
+//DRAW CHEST
+function drawChest(poses) {
+  if (poses) {
+    //Drawing stuff to show chest area
+    noFill();
+    //noStroke();
+    strokeWeight(2);
+    quad(poses[0].pose.rightShoulder.x, poses[0].pose.rightShoulder.y, poses[0].pose.leftShoulder.x, poses[0].pose.leftShoulder.y, poses[0].pose.leftHip.x, poses[0].pose.leftHip.y, poses[0].pose.rightHip.x, poses[0].pose.rightHip.y);
+  }
+
+  //Making arrays for x and y of the upper body rectangle points
+  upperX[0] = round(poses[0].pose.rightHip.x);
+  upperX[1] = round(poses[0].pose.leftHip.x);
+  upperY[0] = round(poses[0].pose.leftShoulder.y);
+  upperY[1] = round(poses[0].pose.leftHip.y-(upperX[1]-upperX[0]));
+
+  //Indexes for top left and bottom right pixels in chest area for the pixels array
+  let pixelIdx1 = (upperX[0] + upperY[0] * width) * 4;
+  let pixelIdx2 = (upperX[1] + upperY[1] * width) * 4;
+
+  //Looping in the pixels array and excluding pixels outside the rectangle region
+  let count = 0;
+  let count2 = 0;
+  let i;
+  for (i = pixelIdx1; i < pixelIdx2; i+=4) {
+    if (count < (upperX[1] - upperX[0])) {
+      //Adding the red, green, and blue values
+      avgRedUpper += img.pixels[i + 0];
+      avgGreenUpper += img.pixels[i + 1];
+      avgBlueUpper += img.pixels[i + 2];
+      count += 1;
+    }
+
+    else {
+      count = 0;
+      i += (width - (upperX[1]-upperX[0]))*4  ;
+      count2 += 1;
+    }
+  }
+
+  // Get the total number of pixels
+  const numPixelsUpper = (upperX[1]-upperX[0])*(upperY[1]-upperY[0]);
+
+  // divide the totals by the number of pixels to find the average.
+  avgRedUpper /= numPixelsUpper;
+  avgGreenUpper /= numPixelsUpper;
+  avgBlueUpper /= numPixelsUpper;
+
+  fill(avgRedUpper, avgGreenUpper, avgBlueUpper);
+  // Draw a square in the center of the screen
+  rectMode(CENTER);
+  rect(width / 2, height / 2, 100, 100);
+}
+
+//DRAW LEGS
+function drawLegs(poses) {
+  if (poses) {
+    //Drawing stuff to show legs area
+    fill(255);
+    ellipse(poses[0].pose.rightHip.x, poses[0].pose.rightHip.y, 10);
+    ellipse(poses[0].pose.leftHip.x, poses[0].pose.leftHip.y, 10);
+    ellipse(poses[0].pose.rightKnee.x, poses[0].pose.rightKnee.y, 10);
+    ellipse(poses[0].pose.leftKnee.x, poses[0].pose.leftKnee.y, 10);
+  }
+
+  //Making arrays for x and y of the lower body rectangle points
+  lowerX[0] = round(poses[0].pose.rightHip.x);
+  lowerY[0] = round(poses[0].pose.rightHip.y);
+  lowerY[1] = round(poses[0].pose.rightKnee.y);
+
+  //Indexes for top left and bottom right pixels in legs area for the pixels array
+  let pixelIdx1 = (lowerX[0] + lowerY[0] * width) * 4;
+  let pixelIdx2 = (lowerX[0] + lowerY[1] * width) * 4;
+
+  console.log(pixelIdx1);
+  console.log(pixelIdx2);
+
+  //Looping in the pixels array and excluding pixels outside the rectangle region
+  for (let i = pixelIdx1; i < pixelIdx2; i++) {
+      //Adding the red, green, and blue values
+      avgRedLower += img.pixels[i + 0];
+      avgGreenLower += img.pixels[i + 1];
+      avgBlueLower += img.pixels[i + 2];
+      i += (width * 4) - 1;
+    }
+
+  // Get the total number of pixels
+  const numPixelsLower = abs(lowerY[0]-lowerY[1]);
+
+  // divide the totals by the number of pixels to find the average.
+  avgRedLower /= numPixelsLower;
+  avgGreenLower /= numPixelsLower;
+  avgBlueLower /= numPixelsLower;
+
+
+  console.log(avgBlueLower)
+  console.log(numPixelsLower);
+
+  fill(avgRedLower, avgGreenLower, avgBlueLower);
+  // Draw a square in the center of the screen
+  rectMode(CENTER);
+  rect(250, 250, 100, 100);
+
+}
+
+//DRAW
+function draw() {
+
+}
